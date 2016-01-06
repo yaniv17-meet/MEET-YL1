a980fdd6f5f35debaa5b3dae73b798faa4968c14
neta <netarvd@gmail.com>
2016-01-06 20:05:43 +0200

    PS

---
+++ isPalindrome.py
@@ -0,0 +1,8 @@
+n = input("Enter smth: ")
+print  (n)
+print  (n[::-1])
+if n == n[::-1]:
+	print ("It is a plindrome")
+else:
+	print ("It isn't a palindrome")
+
---
+++ randomness.py
@@ -0,0 +1,8 @@
+import random
+print randomint(0,1)
+while(1):
+	n = input("Enter smth")
+	if n == "n":
+		r = randomint(0,1)		
+	else:
+		
